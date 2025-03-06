20a0cddf14968fea4af7b0e0b87772f42023cc05
shez2108 <75090807+shez2108@users.noreply.github.com>
2025-03-06 06:21:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import random
 import string
 import time
 
-
 # Add email input
 st.title("Tool Name")
 email = st.text_input("Enter your email address:")
@@ -15,21 +14,21 @@ if password != "password":
 # Create tabs
 tab1, tab2, tab3 = st.tabs(["Run Domain Analysis", "Merge Data for Reports", "Search Knowledge Base"])
 
-
-    st.write(
-        "Input the domain and competitors of the site you'd like to analyse:"
-    )
-
+with tab1:
+    st.write("Input the domain and competitors of the site you'd like to analyse:")
+    
     domain1 = st.text_input('Enter the domain you want to analyse:')
     competitor_status = st.selectbox('Would you like to get competitor data?', ['No', 'Yes'])
 
     competitor_data = False
     domains = [domain1]
+    
     if competitor_status == 'Yes':
         competitor_data = True
         num_competitors = st.number_input('How many competitors would you like to analyze?', min_value=1, max_value=5, value=2, step=1)
         domains = [domain1]  # Start with the main domain
         competitor_domains = []
+        
         for i in range(5):
             key = f'competitor_{i}'
             if i < num_competitors:
@@ -38,16 +37,15 @@ tab1, tab2, tab3 = st.tabs(["Run Domain Analysis", "Merge Data for Reports", "Se
                     domains.append(competitor_domain)
             else:
                 st.text_input(f'Competitor {i+1}', value='', key=key, disabled=True, label_visibility='hidden')
+        
         domains.extend(competitor_domains)
+        
         if len(domains) > 1:
             competitors_str = ', '.join(domains[1:])
-            st.write(f'Getting relevance data for {domain1} and its competitors: {competitors_str}')
+            st.write(f'Getting data for {domain1} and its competitors: {competitors_str}')
         else:
             st.write('No valid competitor domains have been entered.')
-    else:
-        domains = [domain1]
     
-
     if st.button('Start Analysis'):
         st.write('Running Crawl for', domain1, domains)
-        
+
