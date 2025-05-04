428ca44c9e6b1b9a38e8fc7944164b4abc5946ab
shez2108 <75090807+shez2108@users.noreply.github.com>
2025-05-04 15:26:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,17 @@ client = OpenAI(
     # This is the default and can be omitted
     api_key=api_key,
 )
+
+# take a query input
+query = st.text_input('Type your primary LLM Query here:')
+
+num_queries = st.number_input('How many queries do you want to check brand mentions across? (Up to 100)')
+
+
+if num_queries > 100:
+    st.write('Sorry, you can't look at more than 100 queries')
+    num_queries = st.text_input('How many queries do you want to check brand mentions across? (Up to 100)')
+    num_queries = int(num_queries)
+
+st.write(f'Getting query results for {num_queries} queries.')
+
