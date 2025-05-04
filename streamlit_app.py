f469487094bcd11fd853c5f165fbef344a91600e
shez2108 <75090807+shez2108@users.noreply.github.com>
2025-05-04 20:58:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ from json import JSONDecodeError
 from google.cloud import language_v1
 import os 
 from google.oauth2 import service_account
+import time
 
 st.set_page_config(
     page_title="Query-based Matching for Chat SEO",
